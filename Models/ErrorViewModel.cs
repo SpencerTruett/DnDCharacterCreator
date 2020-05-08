2357f14dfb12929de05d607bce11f553920ad6f3
Spencer Truett <srtruett1993@gmail.com>
2020-05-08 18:56:08 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DnDCharacterCreator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
